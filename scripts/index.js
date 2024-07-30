32805ec4d5f609967c41c3707a294ed9859e862f
Alena <92329824+poletala@users.noreply.github.com>
2024-07-31 01:04:57 +0500

    latlon

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const getMoscowWeatherButton = document.querySelector('#get-moscow-weather')
 const showLocationButton = document.querySelector('#show-location')
-
+const getWeatherByMyLocationButton = document.querySelector('#get-weather-by-my-location')
 
 
 getMoscowWeatherButton.addEventListener('click', () => {
@@ -18,9 +18,32 @@ showLocationButton.addEventListener('click', () => {
     }, (err) => {console.log(err)})
 })
 
+getWeatherByMyLocationButton.addEventListener('click', () => {
+    const onSuccessLocation = (position) => {
+        getWeatherByLocation(position.coords.latitude, position.coords.longitude)
+        .then(data => {
+            console.log(data)
+            console.log('First weather timestamp: ', new DataTransfer(data.list[0].dt))
+        })
+    }
+
+    const onErrorLocation = (err) => {
+        console.log(err)
+    }
+    navigator.geolocation.getCurrentPosition(onSuccessLocation, onErrorLocation)
+})
 function getWeather(city) {
     const API_KEY = "6fbeb34c010c544f6f33fa8071fc677a"
     return appFetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&cnt=${'5'}&units=${'metric'}`)
+}
+
+function getWeatherByLocation(lat, lon) {
+    const API_KEY = "6fbeb34c010c544f6f33fa8071fc677a"
+    return appFetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&cnt=${'5'}&units=${'metric'}`)
+}
+
+function getWeather() {
+
 }
 
 function appFetch(url, options) {
